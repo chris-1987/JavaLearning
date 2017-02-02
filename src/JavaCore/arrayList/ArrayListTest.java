54aa87df91c434fc1bd470a51245d74fd201233b
Yi Wu <wu.yi.christian@gmail.com>
2017-02-02 21:06:40 +0800

    2017/02/02

---
+++ src/JavaCore/arrayList/ArrayListTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaCore.arrayList;
+
+/**
+ *
+ * @author ywu
+ */
+import java.util.*;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+    
+        ArrayList<Employee> staff = new ArrayList<>();
+        
+        staff.add(new Employee("Carl Cracker", 75000, 1987, 12, 15));
+        
+        staff.add(new Employee("Harry Hacker", 50000, 1989, 10, 1));
+        
+        staff.add(new Employee("Tony Tester", 40000, 1990, 3, 15));
+        
+        for (Employee e: staff) 
+            e.raiseSalary(5);
+        
+        for (Employee e : staff)
+            System.out.println("name = "  + e.getName + ", salary = "  + e.getSalary() + ", hireDay = " + e.getHireDay());
+    }
+    
+}
---
+++ src/JavaCore/reflection/ReflectionTest.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaCore.reflection;
+
+/**
+ *
+ * @author ywu
+ */
+import java.util.Scanner;
+import java.lang.reflect.*;
+
+public class ReflectionTest {
+
+    public static void main(String[] args) {
+
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            Scanner in = new Scanner(System.in);
+
+            System.out.println("Enter class name: ");
+
+            name = in.next();
+
+            try {
+
+                Class cl = Class.forName(name);
+
+                Class supercl = cl.getSuperclass();
+
+                String modifiers = Modifier.toString(cl.getModifiers());
+
+                if (modifiers.length() > 0) {
+                    System.out.print(modifiers + " ");
+                }
+
+                System.out.print("class " + name);
+
+                if (supercl != null && supercl != Object.class) {
+                    System.out.print(" extends " + supercl.getName());
+                }
+
+                System.out.print("\n{\n");
+
+                printConstructors(cl);
+
+                System.out.println();
+
+                printMethods(cl);
+
+                System.out.println();
+
+                printFields(cl);
+
+                System.out.println("}");
+            } catch (ClassNotFoundException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void printConstructors(Class cl) {
+
+        Constructor[] constructors = cl.getDeclaredConstructors();
+
+        for (Constructor c : constructors) {
+
+            String name = c.getName();
+
+            System.out.print(" ");
+
+            String modifiers = Modifier.toString(c.getModifiers());
+
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+
+            System.out.print(name + "(");
+
+            Class[] paramTypes = c.getParameterTypes();
+
+            for (int j = 0; j < paramTypes.length; ++j) {
+
+                if (j > 0) {
+                    System.out.print(", ");
+                }
+
+                System.out.print(paramTypes[j].getName());
+            }
+
+            System.out.println(");");
+        }
+    }
+
+    public static void printFields(Class cl) {
+
+        Method[] methods = cl.getDeclaredMethods();
+
+        for (Method m : methods) {
+
+            Class retType = m.getReturnType();
+
+            String name = m.getName();
+
+            System.out.print(" ");
+
+            String modifiers = Modifier.toString(m.getModifiers());
+
+            if (modifiers.length() > 0) {
+
+                System.out.print(modifiers + " ");
+            }
+
+            System.out.print(retType.getName() + " " + name + "(");
+
+            Class[] paramTypes = m.getParameterTypes();
+
+            for (int j = 0; j < paramTypes.length; ++j) {
+
+                if (j > 0) {
+                    System.out.print(", ");
+                }
+
+                System.out.print(paramTypes[j].getName());
+            }
+
+            System.out.println(");");
+        }
+    }
+
+    public static void printMethods(Class cl) {
+
+        Field[] fields = cl.getDeclaredFields();
+
+        for (Field f : fields) {
+
+            Class type = f.getType();
+
+            String name = f.getName();
+
+            System.out.print("  ");
+
+            String modifiers = Modifier.toString(f.getModifiers());
+
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+
+            System.out.println(type.getName() + " " + name + ";");
+        }
+    }
+
+}
